09b5f8abe276f68f6706e7453deddc00f037fa03
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-01-30 19:26:27 +0000

    Update Variance_covariance_wild.R

--- Variance_covariance_wild.R
+++ Variance_covariance_wild.R
@@ -16,6 +16,31 @@ library(geomorph)
 library(vcvComp)
 library(tidyverse)
 
+## Example data from cichlids
+data("Tropheus")
+
 landmarks = read_csv('allometry minimised data (XY) with ID (6 population pairs).csv')
 
+identifiers = landmarks %>% 
+  select(ID, 
+         POP, 
+         Morph, 
+         CS)
 
+LM_data = landmarks %>% 
+  select(-ID, 
+         -POP, 
+         -Morph, 
+         -CS) %>% 
+  select(-starts_with('LMS'))
+
+phenotypes = as.matrix(LM_data[which(names(LM_data) == 'LM1X'):
+                    which(names(LM_data) == 'LM22Y')])
+
+# dim(phenotypes)
+
+phenotype_array = arrayspecs(phenotypes, p = 22, k = 2)
+
+
+phenotype_gpa = gpagen(phenotype_array, 
+                       print.progress = F)
