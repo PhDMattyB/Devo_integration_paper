582e0ceda15a7d29da12addffb08ad13c5c8d1d9
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-02-07 19:39:57 +0000

    Update Variance_covariance_wild.R

--- Variance_covariance_wild.R
+++ Variance_covariance_wild.R
@@ -1415,6 +1415,55 @@ SKRC_mt = modularity.test(SKRC_gpa$coords,
 summary(SKRC_mt) # Test summary
 plot(SKRC_mt) #
 
+land.gps2 = rep('a', 22); land.gps2[1:2] = 'b'; land.gps2[3:5] = 'c'
+SKRC_mt2 = modularity.test(SKRC_gpa$coords,
+                           land.gps2,
+                           CI=T,
+                           iter=999)
+summary(SKRC_mt2) # Test summary
+# plot(SKRC_mt2) # Histogram of CR sampling distribution
+
+land.gps3 = rep('a', 22); land.gps3[1:2] = 'b'; land.gps3[3:5] = 'c'; land.gps3[6:9] = 'd'
+SKRC_mt3 = modularity.test(SKRC_gpa$coords,
+                           land.gps3,
+                           CI=T,
+                           iter=999)
+summary(SKRC_mt3) # Test summary
+# plot(SKRC_mt3) # Histogram of CR sampling distribution
+
+land.gps4 = rep('a', 22); land.gps4[1:2] = 'b'; land.gps4[3:5] = 'c'; land.gps4[6:9] = 'd'; land.gps4[10:12] = 'e'
+SKRC_mt4 = modularity.test(SKRC_gpa$coords,
+                           land.gps4,
+                           CI=T,
+                           iter=999)
+summary(SKRC_mt4) # Test summary
+# plot(SKRC_mt4) # Histogram of CR sampling distribution
+
+land.gps5 = rep('a', 22); land.gps5[1:2] = 'b'; land.gps5[3:5] = 'c'; land.gps5[6:9] = 'd'; land.gps5[10:12] = 'e'; land.gps5[14:18] = 'f'
+SKRC_mt5 = modularity.test(SKRC_gpa$coords,
+                           land.gps5,
+                           CI=T,
+                           iter=999)
+summary(SKRC_mt5) # Test summary
+# plot(SKRC_mt5) # Histogram of CR sampling distribution
+
+land.gps6 = rep('a', 22); land.gps6[1:2] = 'b'; land.gps6[3:5] = 'c'; land.gps6[6:9] = 'd'; land.gps6[10:12] = 'e'; land.gps6[14:18] = 'f'; land.gps6[20:21] = 'g'
+SKRC_mt6 = modularity.test(SKRC_gpa$coords,
+                           land.gps6,
+                           CI=T,
+                           iter=999)
+summary(SKRC_mt6) # Test summary
+# plot(SKRC_mt6) # Histogram of CR sampling distribution
+
+SKRC_model.Z = compare.CR(SKRC_mt, 
+                          SKRC_mt2,
+                          SKRC_mt3, 
+                          SKRC_mt4, 
+                          SKRC_mt5, 
+                          SKRC_mt6, 
+                          CR.null = TRUE)
+SKRC_model.Z
+
 SKRC_IT = integration.test(SKRC_gpa$coords, 
                             partition.gp=land.gps, 
                             iter=999)
