4c600538f0a0e186848eec1d0ad110c49bd3b756
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-03-04 17:49:36 +0000

    Create Wild_Integration.R

---
+++ Wild_Integration.R
@@ -0,0 +1,16 @@
+##############################
+## Wild fish integration analysis
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 04.03.2024
+##
+##############################
+
+
+setwd('~/Parsons_Postdoc/Stickleback_Morphometric_data/Updated Landmarks/')
+
+library(geomorph)
+library(vcvComp)
+library(factoextra)
+library(tidyverse)
