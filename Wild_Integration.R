7e0eea022c101087a94101a35d7d91ddb1c4d1ef
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-04-03 18:30:02 +0100

    Update Wild_Integration.R

--- Wild_Integration.R
+++ Wild_Integration.R
@@ -38,6 +38,13 @@ allometry_model1 = procD.lm(wild_gpa$coords ~ log(wild_gpa$Csize),
 summary(allometry_model1)
 
 
+shape_resid = arrayspecs(allometry_model1$residuals, 
+                         p = dim(wild_gpa$coords)[1], 
+                         k = dim(wild_gpa$coords)[2])
+allometry_adj_shape = shape_resid + array(wild_gpa$consensus, 
+                                dim(shape_resid))
+
+
 # allometry_model2 = procD.lm(wild_gpa$coords ~ wild_gpa$Csize * identifiers$Lake, 
 #                             iter = 999, 
 #                             RRPP = T)
@@ -56,3 +63,17 @@ allometry_model4 = procD.lm(wild_gpa$coords ~ log(wild_gpa$Csize) * identifiers$
 summary(allometry_model4)
 
 
+
+# PCA ---------------------------------------------------------------------
+
+
+## Need to figure out a way to make this ggplot able
+PCA_allometry = gm.prcomp(wild_gpa$coords)
+plot(PCA_allometry,
+     pch=21,
+     # bg=identifiers$Lake_morph,
+     cex=1.5)
+
+
+PCA_allometry_adj = gm.prcomp(allometry_adj_shape)
+plot(PCA_allometry_adj)
