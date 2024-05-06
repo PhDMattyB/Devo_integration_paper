ae2b2e9b98157c23e2f091223103421a2d5e5fd7
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-05-06 19:46:25 +0100

    Create transgen_int.R

---
+++ transgen_int.R
@@ -0,0 +1,39 @@
+##############################
+## Transgenerational integration models
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 06.05.2024
+##
+##############################
+
+setwd('~/Parsons_Postdoc/Stickleback_Morphometric_data/Updated Landmarks/')
+
+library(geomorph)
+library(vcvComp)
+library(factoextra)
+library(tidyverse)
+
+F2_tps = readland.tps('F2_No_GT.TPS', 
+                      specID = 'imageID')
+
+identifiers = read_csv('F2_Metadata.CSV', 
+                       col_names = T) 
+
+
+F2_craniofacial = readland.tps('F2_Craniofacial_LM.TPS',
+                               specID = 'imageID')
+
+identifiers = read_csv('F2_metadata.csv') %>% 
+  unite('Ecotype_Pair_Full_Temp', 
+        Ecotype_pair, 
+        Full_temp, 
+        sep = '_', 
+        remove = F)
+
+
+F2_craniofacial_gpa = gpagen(F2_craniofacial,
+                             print.progress = F)
+
+subset_F2_craniofacial_coords = coords.subset(F2_craniofacial_gpa$coords,
+                                              identifiers$Ecotype_Pair_Full_Temp)
