b4bf20e880ff19db6273d44b06e4f0db4f9bff2d
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-05-13 17:10:08 +0100

    Update F2_CVA.R

--- F2_CVA.R
+++ F2_CVA.R
@@ -65,9 +65,15 @@ F2_geo_df = geomorph.data.frame(coords = two.d.array(F2_craniofacial_gpa$coords)
 # F2_off_temp = lm.rrpp(coords ~ offspring_temp * lake_morph,
 #                      data = F2_geo_df)
 # 
+
+
+# F2 off temp per lake morph CVA per morph -----------------------------------------------
+
+
 F2_off_temp = procD.lm(coords ~ offspring_temp * lake_morph, 
                 data = F2_geo_df)
 
+summary(F2_off_temp)
 
 prep.lda(F2_off_temp, 
          inherent.groups = TRUE) # see groups available
@@ -101,6 +107,9 @@ F2_off_temp_cva = bind_cols(F2_off_temp_cva_scores,
         Offspring_temp, 
         sep = '_', 
         remove = F)
+
+
+# F2 off temp per lake morph data viz ----------------------------------------------------
 
 ## graphing them all together is hard to see
 F2_off_temp_means = F2_off_temp_cva %>% 
@@ -232,3 +241,141 @@ ggplot(data = GTS_CSWY_off_temp,
              aes(col = Ecotype_off_temp, 
                  x = mean_LD1, 
                  y = mean_LD2))
+
+# F2 off temp CVA temp treatment -----------------------------------------------
+
+
+F2_off_temp_only = procD.lm(coords ~ offspring_temp, 
+                       data = F2_geo_df)
+
+summary(F2_off_temp_only)
+
+prep.lda(F2_off_temp_only, 
+         inherent.groups = TRUE) # see groups available
+
+lda.args = prep.lda(F2_off_temp_only) 
+
+CVA = do.call(lda, lda.args)
+CVA # 3 CVs produced
+
+## Axis scaling
+CVA$scaling # will return all CVs
+
+CVA$means
+## divide axis 1 and sum of all axes to get var explained
+CVA$svd
+
+## CVS scores for each individual
+F2_off_temp_only_cva_scores = predict(CVA)
+
+F2_off_temp_only_cva_scores = F2_off_temp_only_cva_scores$x %>% 
+  as.data.frame() %>% 
+  rownames_to_column() %>% 
+  as_tibble() %>% 
+  rename(individualID = rowname) %>% 
+  arrange(individualID)
+
+F2_off_temp_only_cva = bind_cols(F2_off_temp_only_cva_scores, 
+                            identifiers) %>% 
+  unite('Ecotype_off_temp', 
+        Lake_morph, 
+        Offspring_temp, 
+        sep = '_', 
+        remove = F)
+
+F2_off_temp_only_means = F2_off_temp_only_cva %>% 
+  group_by(Ecotype_off_temp) %>% 
+  summarize(mean_LD1 = mean(LD1))
+
+ggplot(data = F2_off_temp_only_cva, 
+       aes(x = LD1)) + 
+  geom_density(aes(col = Ecotype_off_temp, 
+                   fill = Ecotype_off_temp))
+
+
+
+
+# ASHN_off_temp_means = F2_off_temp_only_cva %>% 
+#   group_by(Ecotype_off_temp) %>% 
+#   summarize(mean_LD1 = mean(LD1)) %>% 
+#   filter(Ecotype_off_temp %in% c('ASHNC_12', 
+#                                  'ASHNC_18', 
+#                                  'ASHNW_12', 
+#                                  'ASHNW_18'))
+
+ASHN_off_temp_only = F2_off_temp_only_cva %>% 
+  filter(Ecotype_off_temp %in% c('ASHNC_12', 
+                                 'ASHNC_18', 
+                                 'ASHNW_12', 
+                                 'ASHNW_18'))
+
+ggplot(data = ASHN_off_temp_only, 
+       aes(x = LD1)) +
+  geom_density(aes(col = Ecotype_off_temp, 
+                   fill = Ecotype_off_temp))
+  
+
+# F2_off_temp_means = F2_off_temp_cva %>% 
+#   group_by(Ecotype_off_temp) %>% 
+#   summarize(mean_LD1 = mean(LD1), 
+#             mean_LD2 = mean(LD2)) %>% 
+#   filter(Ecotype_off_temp %in% c('MYVC_12', 
+#                                  'MYVC_18', 
+#                                  'MYVW_12', 
+#                                  'MYVW_18'))
+
+MYV_off_temp_only = F2_off_temp_only_cva %>% 
+  filter(Ecotype_off_temp %in% c('MYVC_12', 
+                                 'MYVC_18', 
+                                 'MYVW_12', 
+                                 'MYVW_18'))
+
+ggplot(data = MYV_off_temp_only, 
+       aes(x = LD1)) +
+  geom_density(aes(col = Ecotype_off_temp, 
+                   fill = Ecotype_off_temp))
+
+
+# F2_off_temp_means = F2_off_temp_cva %>% 
+#   group_by(Ecotype_off_temp) %>% 
+#   summarize(mean_LD1 = mean(LD1), 
+#             mean_LD2 = mean(LD2)) %>% 
+#   filter(Ecotype_off_temp %in% c('SKRC_12', 
+#                                  'SKRC_18', 
+#                                  'SKRW_12', 
+#                                  'SKRW_18'))
+
+SKR_off_temp_only = F2_off_temp_only_cva %>% 
+  filter(Ecotype_off_temp %in% c('SKRC_12', 
+                                 'SKRC_18', 
+                                 'SKRW_12', 
+                                 'SKRW_18'))
+
+ggplot(data = SKR_off_temp_only, 
+       aes(x = LD1)) +
+  geom_density(aes(col = Ecotype_off_temp, 
+                   fill = Ecotype_off_temp))
+
+
+
+# F2_off_temp_means = F2_off_temp_cva %>% 
+#   group_by(Ecotype_off_temp) %>% 
+#   summarize(mean_LD1 = mean(LD1), 
+#             mean_LD2 = mean(LD2)) %>% 
+#   filter(Ecotype_off_temp %in% c('CSWYC_12', 
+#                                  'CSWYC_18', 
+#                                  'GTSW_12', 
+#                                  'GTSW_18'))
+
+GTS_CSWY_off_temp_only = F2_off_temp_only_cva %>% 
+  filter(Ecotype_off_temp %in% c('CSWYC_12', 
+                                 'CSWYC_18', 
+                                 'GTSW_12', 
+                                 'GTSW_18'))
+
+ggplot(data = GTS_CSWY_off_temp_only, 
+       aes(x = LD1)) +
+  geom_density(aes(col = Ecotype_off_temp, 
+                   fill = Ecotype_off_temp))
+
+##
