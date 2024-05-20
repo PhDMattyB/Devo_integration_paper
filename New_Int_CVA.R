0983a4a7e1ad51cabfd67f9c8683e90e5b1f3220
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-05-20 12:38:58 +0100

    Update New_Int_CVA.R

--- New_Int_CVA.R
+++ New_Int_CVA.R
@@ -45,15 +45,44 @@ F2_craniofacial = readland.tps('F2_Craniofacial_LM.TPS',
                                specID = 'imageID')
 F2_craniofacial_gpa = gpagen(F2_craniofacial,
                              print.progress = F)
+F2_cranio_geo_df = geomorph.data.frame(coords = two.d.array(F2_craniofacial_gpa$coords), 
+                                       Full_factor = identifiers$Ecotype_Pair_Full_Temp, 
+                                       parent_temp = identifiers$Parent_temp, 
+                                       offspring_temp = identifiers$Offspring_temp,
+                                       grand_temp = identifiers$Grand_temp,
+                                       morph = identifiers$Morph, 
+                                       population = identifiers$Lake,
+                                       lake_morph = identifiers$Lake_morph,
+                                       lake_morph_full = identifiers$lake_morph_Pair_Full_Temp)
+
 
 F2_whole_body = readland.tps('F2_No_GT.TPS', 
                       specID = 'imageID')
 F2_whole_body_gpa = gpagen(F2_whole_body,
                              print.progress = F)
+F2_WB_geo_df = geomorph.data.frame(coords = two.d.array(F2_whole_body_gpa$coords), 
+                                       Full_factor = identifiers$Ecotype_Pair_Full_Temp, 
+                                       parent_temp = identifiers$Parent_temp, 
+                                       offspring_temp = identifiers$Offspring_temp,
+                                       grand_temp = identifiers$Grand_temp,
+                                       morph = identifiers$Morph, 
+                                       population = identifiers$Lake,
+                                       lake_morph = identifiers$Lake_morph,
+                                       lake_morph_full = identifiers$lake_morph_Pair_Full_Temp)
+
 
 F2_4bar = readland.tps('F2_4bar_linkage.TPS', 
                        specID = 'imageID')
 F2_4bar_gpa = gpagen(F2_4bar,
                              print.progress = F)
+F2_4bar_geo_df = geomorph.data.frame(coords = two.d.array(F2_4bar_gpa$coords), 
+                                       Full_factor = identifiers$Ecotype_Pair_Full_Temp, 
+                                       parent_temp = identifiers$Parent_temp, 
+                                       offspring_temp = identifiers$Offspring_temp,
+                                       grand_temp = identifiers$Grand_temp,
+                                       morph = identifiers$Morph, 
+                                       population = identifiers$Lake,
+                                       lake_morph = identifiers$Lake_morph,
+                                       lake_morph_full = identifiers$lake_morph_Pair_Full_Temp)
 
 
