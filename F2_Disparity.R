d4c4af40f6ab8ee27f00a9f3b5a0e5acdca26bfe
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-05-23 13:35:11 +0100

    Update F2_Disparity.R

--- F2_Disparity.R
+++ F2_Disparity.R
@@ -42,28 +42,74 @@ F2_craniofacial = readland.tps('F2_Craniofacial_LM.TPS',
                                specID = 'imageID')
 F2_craniofacial_gpa = gpagen(F2_craniofacial,
                              print.progress = F)
+F2_cranio_geo_df = geomorph.data.frame(coords = two.d.array(F2_craniofacial_gpa$coords), 
+                                       Full_factor = identifiers$Ecotype_Pair_Full_Temp, 
+                                       parent_temp = identifiers$Parent_temp, 
+                                       offspring_temp = identifiers$Offspring_temp,
+                                       grand_temp = identifiers$Grand_temp,
+                                       morph = identifiers$Morph, 
+                                       population = identifiers$Lake,
+                                       lake_morph = identifiers$Lake_morph,
+                                       lake_morph_full = identifiers$lake_morph_Pair_Full_Temp)
 
 ## body shape data
 F2_body = readland.tps('F2_Body_LM.TPS', 
                        specID = 'imageID')
 F2_body_gpa = gpagen(F2_body,
                      print.progress = F)
+F2_body_geo_df = geomorph.data.frame(coords = two.d.array(F2_body_gpa$coords), 
+                                     Full_factor = identifiers$Ecotype_Pair_Full_Temp, 
+                                     parent_temp = identifiers$Parent_temp, 
+                                     offspring_temp = identifiers$Offspring_temp,
+                                     grand_temp = identifiers$Grand_temp,
+                                     morph = identifiers$Morph, 
+                                     population = identifiers$Lake,
+                                     lake_morph = identifiers$Lake_morph,
+                                     lake_morph_full = identifiers$lake_morph_Pair_Full_Temp)
 
 ## 4 bar linkage
 F2_4bar = readland.tps('F2_4bar_linkage.TPS', 
                        specID = 'imageID')
 F2_4bar_gpa = gpagen(F2_4bar,
                      print.progress = F)
+F2_4bar_geo_df = geomorph.data.frame(coords = two.d.array(F2_4bar_gpa$coords), 
+                                     Full_factor = identifiers$Ecotype_Pair_Full_Temp, 
+                                     parent_temp = identifiers$Parent_temp, 
+                                     offspring_temp = identifiers$Offspring_temp,
+                                     grand_temp = identifiers$Grand_temp,
+                                     morph = identifiers$Morph, 
+                                     population = identifiers$Lake,
+                                     lake_morph = identifiers$Lake_morph,
+                                     lake_morph_full = identifiers$lake_morph_Pair_Full_Temp)
 
 ## eye shape 
 multi_shape_345 = readland.tps('multi_shape_345.TPS', 
                                specID = 'imageID')
 multi_shape_345 = gpagen(multi_shape_345)
+F2_multi_345 = geomorph.data.frame(coords = two.d.array(multi_shape_345$coords), 
+                                   Full_factor = identifiers$Ecotype_Pair_Full_Temp, 
+                                   parent_temp = identifiers$Parent_temp, 
+                                   offspring_temp = identifiers$Offspring_temp,
+                                   grand_temp = identifiers$Grand_temp,
+                                   morph = identifiers$Morph, 
+                                   population = identifiers$Lake,
+                                   lake_morph = identifiers$Lake_morph,
+                                   lake_morph_full = identifiers$lake_morph_Pair_Full_Temp)
+
 
 ## operculum shape
 multi_shape_789 = readland.tps('multi_shape_789.TPS', 
                                specID = 'imageID')
 multi_shape_789 = gpagen(multi_shape_789)
+F2_multi_789 = geomorph.data.frame(coords = two.d.array(multi_shape_789$coords), 
+                                   Full_factor = identifiers$Ecotype_Pair_Full_Temp, 
+                                   parent_temp = identifiers$Parent_temp, 
+                                   offspring_temp = identifiers$Offspring_temp,
+                                   grand_temp = identifiers$Grand_temp,
+                                   morph = identifiers$Morph, 
+                                   population = identifiers$Lake,
+                                   lake_morph = identifiers$Lake_morph,
+                                   lake_morph_full = identifiers$lake_morph_Pair_Full_Temp)
 
 
 # ashn = landmarks %>%
