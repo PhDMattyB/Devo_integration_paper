1d6e9072c191ed7619015b2b0218e0c59636a1dd
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-05-23 13:30:08 +0100

    Create F2_Disparity.R

---
+++ F2_Disparity.R
@@ -0,0 +1,66 @@
+#################################################
+## Disparity analyses - ecotypes and environments
+##
+## Matt Brachmann (PhDMattyB)
+##
+##  23.05.2024
+## 
+################################################
+
+
+setwd('~/Parsons_Postdoc/Stickleback_Morphometric_data/Updated Landmarks/')
+
+
+library(geomorph)
+library(RRPP)
+# library(MASS)
+# library(ppcor)
+# library(igraph)
+library(tidyverse)
+
+identifiers = read_csv('F2_metadata.csv') %>% 
+  rename(individualID = Names) %>% 
+  unite('lake_morph_Pair_Full_Temp', 
+        Lake_morph, 
+        Full_temp, 
+        sep = '_', 
+        remove = F) %>% 
+  unite('Ecotype_Pair_Full_Temp', 
+        Ecotype_pair, 
+        Full_temp, 
+        sep = '_', 
+        remove = F) %>% 
+  mutate(across(c('Lake_morph',
+                  'Offspring_temp',
+                  'Parent_temp',
+                  'Grand_temp'),
+                factor)) %>% 
+  arrange(individualID)
+
+## Craniofacial traits
+F2_craniofacial = readland.tps('F2_Craniofacial_LM.TPS',
+                               specID = 'imageID')
+F2_craniofacial_gpa = gpagen(F2_craniofacial,
+                             print.progress = F)
+
+## body shape data
+F2_body = readland.tps('F2_Body_LM.TPS', 
+                       specID = 'imageID')
+F2_body_gpa = gpagen(F2_body,
+                     print.progress = F)
+
+## 4 bar linkage
+F2_4bar = readland.tps('F2_4bar_linkage.TPS', 
+                       specID = 'imageID')
+F2_4bar_gpa = gpagen(F2_4bar,
+                     print.progress = F)
+
+## eye shape 
+multi_shape_345 = readland.tps('multi_shape_345.TPS', 
+                               specID = 'imageID')
+multi_shape_345 = gpagen(multi_shape_345)
+
+## operculum shape
+multi_shape_789 = readland.tps('multi_shape_789.TPS', 
+                               specID = 'imageID')
+multi_shape_789 = gpagen(multi_shape_789)
