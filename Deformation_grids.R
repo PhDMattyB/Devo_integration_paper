58ddd613881182d77f41f3883f431c50e547d458
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-06-06 14:32:32 +0100

    Update Deformation_grids.R

--- Deformation_grids.R
+++ Deformation_grids.R
@@ -54,6 +54,9 @@ F2_orig_ordered = F2_oringial_data[order(F2_original_data$Lake_morph),]  # order
 
 rownames(F2_orig_ordered) <- as.integer(1:931) #name rows to make it easier to organise
 
+
+# ASHN original trait deformation grids -----------------------------------
+
 ASHNC_orig_shape = F2_orig_ordered[1:121,]
 ASHNW_orig_shape = F2_orig_ordered[122:243,]
 
@@ -64,6 +67,115 @@ ASHNC_orig_mean = mshape(ASHNC_orig_shape)
 ASHNW_orig_shape = arrayspecs(ASHNW_orig_shape, 27, 2)
 ASHNW_orig_mean = mshape(ASHNW_orig_shape)
 
-links <- cbind(c(1,2,7,8,8,9), c(2,7,8,9,13,10))
-LFBen <- gridPar(tar.pt.size=0.8, tar.pt.bg = "green", tar.link.col="black", tar.link.lwd = 2.5,grid.col='darkgreen', grid.lty = 10, n.col.cell = 30)
-plotRefToTarget(LFLimneticMean, LFBenthicMean, mag=4, links=links, gridPars=LFBen)
+####plotRefToTarget (reference, target species)
+
+links <- cbind(c(1,2,6,12,13,14,15,16,17,18,19,20,21,22,2,23,23,24,8,25,10,8,7,7), 
+               c(2,6,12,13,14,15,16,17,18,19,20,21,22,1,23,27,24,8,27,26,11,9,8,9))
+ASHNC_def <- gridPar(tar.pt.size=0.8, tar.pt.bg = "green", tar.link.col="black", tar.link.lwd = 2.5,grid.col='darkgreen', grid.lty = 10, n.col.cell = 30)
+plotRefToTarget(ASHNW_orig_mean, 
+                ASHNC_orig_mean, 
+                mag=4, 
+                links=links, 
+                gridPars=ASHNC_def)
+
+ASHNW_def <- gridPar(tar.pt.size=0.8, tar.pt.bg = "green", tar.link.col="black", tar.link.lwd = 2.5,grid.col='darkgreen', grid.lty = 10, n.col.cell = 30)
+plotRefToTarget(ASHNC_orig_mean, 
+                ASHNW_orig_mean, 
+                mag=4, 
+                links=links, 
+                gridPars=ASHNC_def)
+
+
+
+# ASHN F1 effect deformation grids ----------------------------------------
+F1_effect_tps = readland.tps('F1_Corrected_landmarks.tps', 
+                             specID = 'imageID')
+
+F1_effect_gpa = gpagen(F1_effect_tps, 
+                print.progress = F)
+
+
+F1_effect_data = two.d.array(F1_effect_gpa$coords) # get coords into an array
+F1_effect_data = cbind(identifiers, F1_effect_data) # bind with the variables; all in the correct order
+
+F1_effect_ordered = F1_effect_data[order(F1_effect_data$Lake_morph),]  # order to make it easier to split the dataset
+
+rownames(F1_effect_ordered) <- as.integer(1:931) #name rows to make it easier to organise
+
+
+ASHNC_F1_shape = F1_effect_ordered[1:121,]
+ASHNW_F1_shape = F1_effect_ordered[122:243,]
+
+ASHNC_F1_shape = ASHNC_F1_shape[,-c(1:12)]
+ASHNC_F1_shape = arrayspecs(ASHNC_F1_shape, 27, 2)
+ASHNC_F1_mean = mshape(ASHNC_F1_shape)
+
+ASHNW_F1_shape = ASHNW_F1_shape[,-c(1:12)]
+ASHNW_F1_shape = arrayspecs(ASHNW_F1_shape, 27, 2)
+ASHNW_F1_mean = mshape(ASHNW_F1_shape)
+
+####plotRefToTarget (reference, target species)
+
+links <- cbind(c(1,2,6,12,13,14,15,16,17,18,19,20,21,22,2,23,23,24,8,25,10,8,7,7), 
+               c(2,6,12,13,14,15,16,17,18,19,20,21,22,1,23,27,24,8,27,26,11,9,8,9))
+ASHNC_F1_def <- gridPar(tar.pt.size=0.8, tar.pt.bg = "green", tar.link.col="black", tar.link.lwd = 2.5,grid.col='darkgreen', grid.lty = 10, n.col.cell = 30)
+plotRefToTarget(ASHNW_F1_mean, 
+                ASHNC_F1_mean, 
+                mag=3, 
+                links=links, 
+                gridPars=ASHNC_F1_def)
+
+ASHNW_F1_def <- gridPar(tar.pt.size=0.8, tar.pt.bg = "green", tar.link.col="black", tar.link.lwd = 2.5,grid.col='darkgreen', grid.lty = 10, n.col.cell = 30)
+plotRefToTarget(ASHNC_F1_mean, 
+                ASHNW_F1_mean, 
+                mag=4, 
+                links=links, 
+                gridPars=ASHNC_F1_def)
+
+
+# ASHN F2 effect deformation grids ----------------------------------------
+F2_effect_tps = readland.tps('F2_Corrected_landmarks.tps', 
+                             specID = 'imageID')
+
+F2_effect_gpa = gpagen(F2_effect_tps, 
+                       print.progress = F)
+
+
+F2_effect_data = two.d.array(F2_effect_gpa$coords) # get coords into an array
+F2_effect_data = cbind(identifiers, F2_effect_data) # bind with the variables; all in the correct order
+
+F2_effect_ordered = F2_effect_data[order(F2_effect_data$Lake_morph),]  # order to make it easier to split the dataset
+
+rownames(F2_effect_ordered) <- as.integer(1:931) #name rows to make it easier to organise
+
+
+ASHNC_F2_shape = F2_effect_ordered[1:121,]
+ASHNW_F2_shape = F2_effect_ordered[122:243,]
+
+ASHNC_F2_shape = ASHNC_F2_shape[,-c(1:12)]
+ASHNC_F2_shape = arrayspecs(ASHNC_F2_shape, 27, 2)
+ASHNC_F2_mean = mshape(ASHNC_F2_shape)
+
+ASHNW_F2_shape = ASHNW_F2_shape[,-c(1:12)]
+ASHNW_F2_shape = arrayspecs(ASHNW_F2_shape, 27, 2)
+ASHNW_F2_mean = mshape(ASHNW_F2_shape)
+
+####plotRefToTarget (reference, target species)
+
+links <- cbind(c(1,2,6,12,13,14,15,16,17,18,19,20,21,22,2,23,23,24,8,25,10,8,7,7), 
+               c(2,6,12,13,14,15,16,17,18,19,20,21,22,1,23,27,24,8,27,26,11,9,8,9))
+ASHNC_F2_def <- gridPar(tar.pt.size=0.8, tar.pt.bg = "green", tar.link.col="black", tar.link.lwd = 2.5,grid.col='darkgreen', grid.lty = 10, n.col.cell = 30)
+plotRefToTarget(ASHNW_F2_mean, 
+                ASHNC_F2_mean, 
+                mag=3, 
+                links=links, 
+                gridPars=ASHNC_F2_def)
+
+ASHNW_F2_def <- gridPar(tar.pt.size=0.8, tar.pt.bg = "green", tar.link.col="black", tar.link.lwd = 2.5,grid.col='darkgreen', grid.lty = 10, n.col.cell = 30)
+plotRefToTarget(ASHNC_F2_mean, 
+                ASHNW_F2_mean, 
+                mag=4, 
+                links=links, 
+                gridPars=ASHNC_F2_def)
+
+
