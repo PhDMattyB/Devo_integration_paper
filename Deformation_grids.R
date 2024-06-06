6e07d593183708797e9958380f171a3e00c629c0
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-06-06 12:14:05 +0100

    Update Deformation_grids.R

--- Deformation_grids.R
+++ Deformation_grids.R
@@ -50,9 +50,20 @@ F2_gpa = gpagen(F2_tps,
 F2_original_data = two.d.array(F2_gpa$coords) # get coords into an array
 F2_original_data = cbind(identifiers, F2_original_data) # bind with the variables; all in the correct order
 
-F2_orig_ordered <- F2_oringial_data[order(F2_original_data$Lake_morph),]  # order to make it easier to split the dataset
+F2_orig_ordered = F2_oringial_data[order(F2_original_data$Lake_morph),]  # order to make it easier to split the dataset
 
-# rownames(F2_orig_ordered) <- as.integer(1:931) #name rows to make it easier to organise
+rownames(F2_orig_ordered) <- as.integer(1:931) #name rows to make it easier to organise
 
+ASHNC_orig_shape = F2_orig_ordered[1:121,]
+ASHNW_orig_shape = F2_orig_ordered[122:243,]
 
+# test = ASHNC_orig_shape[,-c(1:3)]
+ASHNC_orig_shape = arrayspecs(ASHNC_orig_shape, 27, 2)
+ASHNC_orig_mean = mshape(ASHNC_orig_shape)
 
+ASHNW_orig_shape = arrayspecs(ASHNW_orig_shape, 27, 2)
+ASHNW_orig_mean = mshape(ASHNW_orig_shape)
+
+links <- cbind(c(1,2,7,8,8,9), c(2,7,8,9,13,10))
+LFBen <- gridPar(tar.pt.size=0.8, tar.pt.bg = "green", tar.link.col="black", tar.link.lwd = 2.5,grid.col='darkgreen', grid.lty = 10, n.col.cell = 30)
+plotRefToTarget(LFLimneticMean, LFBenthicMean, mag=4, links=links, gridPars=LFBen)
