e411215f0a10c394c0ec4c3b7fc64630e66fb715
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-08-23 17:17:21 +0100

    dicking around

--- vcvComp_transgen.R
+++ vcvComp_transgen.R
@@ -12,8 +12,78 @@ setwd('~/Parsons_Postdoc/Stickleback_Morphometric_data/Updated Landmarks/')
 library(geomorph)
 library(vcvComp)
 
-wild_univariate = read_csv('Wild_Univariate_traits.csv')
 
-F2_parental_effects = read_csv('F1_Plasticity_Corrected.csv')
+# Metadata ----------------------------------------------------------------
+F2_identifiers = read_csv('F2_metadata.csv') %>% 
+  rename(individualID = Names) %>% 
+  unite('lake_morph_Pair_Full_Temp', 
+        Lake_morph, 
+        Full_temp, 
+        sep = '_', 
+        remove = F) %>% 
+  unite('Ecotype_Pair_Full_Temp', 
+        Ecotype_pair, 
+        Full_temp, 
+        sep = '_', 
+        remove = F) %>% 
+  mutate(across(c('Lake_morph',
+                  'Offspring_temp',
+                  'Parent_temp',
+                  'Grand_temp'),
+                factor)) %>%
+  select(individualID, 
+         Lake, 
+         Ecotype_pair, 
+         Morph, 
+         Lake_morph)
 
-F2_offspring_effects = read_csv('F2_Corrected_F2_temp_only.csv')
+
+# Univariate trait data ---------------------------------------------------
+
+
+wild_univariate = read_csv('Wild_Univariate_traits.csv') %>% 
+  select(-Order, 
+         -rowname) %>% 
+  select(ImageID, 
+         Lake, 
+         Morph, 
+         Lake_morph, 
+         everything()) %>% 
+  rename(individualID = ImageID) 
+
+F2_parental_effects = read_csv('F1_Plasticity_Corrected.csv') %>% 
+  select(-Order, 
+         -rowname, 
+         -Lake, 
+         -Ecotype_Pair_Full_Temp, 
+         -lake_morph_Pair_Full_Temp, 
+         -Grand_temp, 
+         -Parent_temp, 
+         -Offspring_temp, 
+         -Full_temp) %>% 
+  select(individualID, 
+         Ecotype_pair, 
+         Morph, 
+         Lake_morph,
+         everything()) %>% 
+  rename(Lake = Ecotype_pair)
+
+# F2_parental_effects = bind_cols(F2_identifiers, 
+#                                 F2_parental_effects)
+
+F2_offspring_effects = read_csv('F2_Corrected_F2_temp_only.csv')%>% 
+  select(-Order, 
+         -rowname, 
+         -Lake, 
+         -Ecotype_Pair_Full_Temp, 
+         -lake_morph_Pair_Full_Temp, 
+         -Grand_temp, 
+         -Parent_temp, 
+         -Offspring_temp, 
+         -Full_temp) %>% 
+  select(individualID, 
+         Ecotype_pair, 
+         Morph, 
+         Lake_morph,
+         everything())
+
