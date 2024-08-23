8b7179ad613a14c21aae203555f3347f7aa0a01f
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-08-23 16:36:19 +0100

    Create vcvComp_transgen.R

---
+++ vcvComp_transgen.R
@@ -0,0 +1,15 @@
+##############################
+## vcvComp transgen integration
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 23.08.2024
+##
+##############################
+
+setwd('~/Parsons_Postdoc/Stickleback_Morphometric_data/Updated Landmarks/')
+
+library(geomorph)
+library(vcvComp)
+
+wild_univariate = read_csv('')
