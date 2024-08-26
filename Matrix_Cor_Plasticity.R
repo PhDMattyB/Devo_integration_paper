e81a5452913d682fec2f9f4946c600eaf526864d
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-08-26 16:27:02 +0100

    Update Matrix_Cor_Plasticity.R

--- Matrix_Cor_Plasticity.R
+++ Matrix_Cor_Plasticity.R
@@ -10,8 +10,45 @@
 
 setwd('~/Parsons_Postdoc/Stickleback_Morphometric_data/Updated Landmarks/')
 
+# install.packages('MatrixCorrelation')
+library(MatrixCorrelation)
+
 ## Run the plasticity_analysis.R script to get the 
 ## corrected trait correlations
 
+## if this next object doesn't run, re run the script
+## Plasticity due to offspring temperature 
 off_plasticity_trait_cor
 
+off_plast_ASHNW = off_plasticity_trait_cor$ASHNW
+off_plast_ASHNC = off_plasticity_trait_cor$ASHNC
+
+allCorrelations(X1 = off_plast_ASHNW, 
+                X2 = off_plast_ASHNC, 
+                ncomp1 = 28,
+                ncomp2 = 28)
+
+off_plast_MYVW = off_plasticity_trait_cor$MYVW
+off_plast_MYVC = off_plasticity_trait_cor$MYVC
+
+
+allCorrelations(X1 = off_plast_MYVW, 
+                X2 = off_plast_MYVC, 
+                ncomp1 = 28,
+                ncomp2 = 28)
+
+off_plast_SKRW = off_plasticity_trait_cor$SKRW
+off_plast_SKRC = off_plasticity_trait_cor$SKRC
+
+allCorrelations(X1 = off_plast_SKRW, 
+                X2 = off_plast_SKRC, 
+                ncomp1 = 28,
+                ncomp2 = 28)
+
+off_plast_GTSW = off_plasticity_trait_cor$GTSW
+off_plast_CSWYC = off_plasticity_trait_cor$CSWYC
+
+allCorrelations(X1 = off_plast_GTSW, 
+                X2 = off_plast_CSWYC, 
+                ncomp1 = 28,
+                ncomp2 = 28)
