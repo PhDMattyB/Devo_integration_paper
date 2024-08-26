0f2c38bb9b5d541e2bb65d58bcd4447fd174298a
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-08-26 16:19:13 +0100

    Create Matrix_Cor_Plasticity.R

---
+++ Matrix_Cor_Plasticity.R
@@ -0,0 +1,17 @@
+##############################
+## matrix correlation - plasticity
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 26.08.2024
+##
+##############################
+
+
+setwd('~/Parsons_Postdoc/Stickleback_Morphometric_data/Updated Landmarks/')
+
+## Run the plasticity_analysis.R script to get the 
+## corrected trait correlations
+
+off_plasticity_trait_cor
+
