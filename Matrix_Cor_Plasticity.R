b4099dbb90762611f970082ee0ffc5e937f1ab6d
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-08-26 16:29:19 +0100

    Update Matrix_Cor_Plasticity.R

--- Matrix_Cor_Plasticity.R
+++ Matrix_Cor_Plasticity.R
@@ -58,4 +58,36 @@ allCorrelations(X1 = off_plast_GTSW,
 
 # Mat cor F1 parental effect -------------------------------------------------
 
+parent_plasticity_trait_cor
 
+parent_plast_ASHNW = parent_plasticity_trait_cor$ASHNW
+parent_plast_ASHNC = parent_plasticity_trait_cor$ASHNC
+
+allCorrelations(X1 = parent_plast_ASHNW, 
+                X2 = parent_plast_ASHNC, 
+                ncomp1 = 28,
+                ncomp2 = 28)
+
+parent_plast_MYVW = parent_plasticity_trait_cor$MYVW
+parent_plast_MYVC = parent_plasticity_trait_cor$MYVC
+
+
+allCorrelations(X1 = parent_plast_MYVW, 
+                X2 = parent_plast_MYVC, 
+                ncomp1 = 28,
+                ncomp2 = 28)
+
+parent_plast_SKRC = parent_plasticity_trait_cor$SKRC
+
+allCorrelations(X1 = parent_plast_SKRW, 
+                X2 = parent_plast_SKRC, 
+                ncomp1 = 28,
+                ncomp2 = 28)
+
+parent_plast_GTSW = parent_plasticity_trait_cor$GTSW
+parent_plast_CSWYC = parent_plasticity_trait_cor$CSWYC
+
+allCorrelations(X1 = parent_plast_GTSW, 
+                X2 = parent_plast_CSWYC, 
+                ncomp1 = 28,
+                ncomp2 = 28)
