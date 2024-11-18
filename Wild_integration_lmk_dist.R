fbee7844c376dd76fb673b2b8e5d3d9c012636c1
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-11-18 17:24:13 +0000

    holy shit that actually worked

--- Wild_integration_lmk_dist.R
+++ Wild_integration_lmk_dist.R
@@ -12,58 +12,34 @@ setwd('~/Parsons_Postdoc/Stickleback_Morphometric_data/Updated Landmarks/')
 library(tidyverse)
 library(geomorph)
 
+
+# wild traits mag integration ---------------------------------------------
+
 wild_lmk_dist = read_csv('Wild_Univariate_traits.csv')
 
-lmk_dist = wild_lmk_dist %>% 
+wild_dist = wild_lmk_dist %>% 
   select(2:29)
 
 # lmk_dist = geomorph.data.frame(lmk_dist)
-test_mat = as.matrix(lmk_dist)
-test_array = arrayspecs(test_mat, 14, 2)
+wild_lmk_matrix = as.matrix(wild_dist)
+wild_lmk_array = arrayspecs(wild_lmk_matrix, 14, 2)
 
-test_sub = coords.subset(test_array, 
+wild_lmk_sub = coords.subset(wild_lmk_array, 
               wild_lmk_dist$Lake_morph)
 
 
-vrel_wild_coords = Map(function(x) integration.Vrel(x), 
-                       test_sub)
+vrel_wild_lmkdist = Map(function(x) integration.Vrel(x), 
+                       wild_lmk_sub)
 
-ASHN_compare = compare.ZVrel(vrel_wild_coords$ASHNC, 
-                             vrel_wild_coords$ASHNW)
+ASHN_compare = compare.ZVrel(vrel_wild_lmkdist$ASHNC, 
+                             vrel_wild_lmkdist$ASHNW)
+MYV_compare = compare.ZVrel(vrel_wild_lmkdist$MYVC, 
+                            vrel_wild_lmkdist$MYVW)
 
+SKR_compare = compare.ZVrel(vrel_wild_lmkdist$SKRC, 
+                            vrel_wild_lmkdist$SKRW)
 
-## data needs to be a 3d array to subset the data
-test_array = arrayspecs(wild_univariate_traits, 26, 1)
-
-## the data going in needs to be a 3d array
-coords.subset(wild_univariate_traits, 
-              wild_lmk_dist$Lake_morph)
-
-coord_sub = coords.subset(lmk_dist, 
-                          wild_lmk_dist$Lake_morph)
-
-subset_wild_coords = coords.subset(wild_gpa$coords, 
-                                   wild_identifiers$Lake_morph)
-
-vrel_wild_coords = Map(function(x) integration.Vrel(x), 
-                       subset_wild_coords)
-
-ASHN_compare = compare.ZVrel(vrel_wild_coords$ASHNC, 
-                             vrel_wild_coords$ASHNW)
-
-MYV_compare = compare.ZVrel(vrel_wild_coords$MYVC, 
-                            vrel_wild_coords$MYVW)
-
-SKR_compare = compare.ZVrel(vrel_wild_coords$SKRC, 
-                            vrel_wild_coords$SKRW)
-
-# STN_compare = compare.ZVrel(vrel_wild_coords$STNC, 
-#                             vrel_wild_coords$STNW)
-# 
-# RKLT_compare = compare.ZVrel(vrel_wild_coords$RKLTC, 
-#                             vrel_wild_coords$RKLTW)
-
-GTS_CSWY_compare = compare.ZVrel(vrel_wild_coords$CSWY, 
-                                 vrel_wild_coords$GTS)
+GTS_CSWY_compare = compare.ZVrel(vrel_wild_lmkdist$CSWY, 
+                                 vrel_wild_lmkdist$GTS)
 
 
