6a16fcea55c881631d214525260faab70f35f654
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-12-16 17:54:45 +0000

    Update survivorship.R

--- survivorship.R
+++ survivorship.R
@@ -12,6 +12,7 @@ setwd('~/Parsons_Postdoc/Stickleback_Morphometric_data/Updated Landmarks/')
 library(tidyverse)
 library(ggridges)
 library(viridis)
+library(patchwork)
 
 theme_set(theme_bw())
 
@@ -20,6 +21,40 @@ data = read_csv('convertedeusurvdata.csv') %>%
   mutate(group_treatment = paste(group, 
                                  treatment, sep = "_"))
 data$treatment = as.factor(data$treatment)
+
+
+data_clean = data %>% 
+  mutate(.data = data,
+         group2 = as.factor(case_when(
+           group == 'AshnW' ~ 'ASHN - Warm',
+           group == 'AshnC' ~ 'ASHN - Cold',
+           group == 'SKRW' ~ 'SKR - Warm', 
+           group == 'SKRC' ~ 'SKR - Cold', 
+           group == 'MyvatW' ~ 'MYV - Warm', 
+           group == 'MyvatC' ~ 'MYV - Cold', 
+           group == 'GTS' ~ 'GTS - Warm', 
+           group == 'CAUSEWAY' ~ 'GAR - Cold'))) %>% 
+  mutate(.data = data, 
+         treatment2 = as.factor(case_when(
+           treatment == '1212' ~ '12 @ 12', 
+           treatment == '1218' ~ '12 @ 18', 
+           treatment == '1812' ~ '18 @ 12', 
+           treatment == '1818' ~ '18 @ 18'
+         )))
+
+data_clean$group2 = factor(data_clean$group2, 
+                           levels = c('ASHN - Warm', 
+                                      'ASHN - Cold', 
+                                      'MYV - Warm', 
+                                      'MYV - Cold', 
+                                      'SKR - Warm', 
+                                      'SKR - Cold', 
+                                      'GTS - Warm', 
+                                      'GAR - Cold'))
+  
+
+# whole experiment survival -----------------------------------------------
+
 
 # whole_exp_aov = aov(adjustedSurvWholeExp ~ group_treatment, 
 #                     data = data)
@@ -34,15 +69,42 @@ data$treatment = as.factor(data$treatment)
 
 # TukeyHSD(whole_exp_aov)
 
-data %>% 
-  ggplot(aes(x = adjustedSurvWholeExp, 
-             y = group, 
-             fill = group))+
-  facet_grid(~treatment)+
-  geom_density_ridges()
-# +
-#   scale_fill_viridis_b(discrete=TRUE)
 
+  survive_whole_exp_plot = data_clean %>% 
+    ggplot(aes(x = adjustedSurvWholeExp, 
+             y = group2, 
+             fill = group2))+
+  facet_grid(~treatment2)+
+  geom_density_ridges()+
+  scale_fill_viridis(discrete = T)+
+  labs(x = 'Survival over whole experiment', 
+       title = 'Treatment')+
+  theme(axis.title.y = element_blank(), 
+        axis.title.x = element_text(size = 14), 
+        axis.text = element_text(size = 12),
+        axis.text.x = element_text(angle = 90),
+        panel.grid = element_blank(), 
+        strip.background = element_rect(fill = 'white'), 
+        strip.text = element_text(size = 12, 
+                                  face = 'bold'),
+        plot.title = element_text(hjust = 0.5, 
+                                  face = 'bold', 
+                                  size = 14),
+        legend.position = 'none')
+
+
+  
+  
+  
+  
+  ggsave('~/Parsons_Postdoc/Written_things/Stickle_genomic_paper/Fat_Phenotype.tiff', 
+         plot = lipid_plot, 
+         dpi = 'retina', 
+         units = 'cm', 
+         width = 15, 
+         height = 10)
+  
+  
 # 12@12 treatments --------------------------------------------------------
 
 T1212 = data %>% 
@@ -89,5 +151,26 @@ whole_exp_1818_aov = aov(adjustedSurvWholeExp ~ group,
 summary(whole_exp_1818_aov)
 
 TukeyHSD(whole_exp_1818_aov)
+
+
+
+# EU survival -------------------------------------------------------------
+
+EU_data = data %>% 
+  rename(EU_survival = longdata...8)
+
+
+eu_survive_aov = aov(Eusurvivalafter6weeks ~ EU_survival*treatment,
+                    data = EU_data)
+summary(eu_survive_aov)
+
+car::Anova(eu_survive_aov, 
+           type = 'II')
+
+TukeyHSD(eu_survive_aov)
+
+
+
+# 6 week survival ---------------------------------------------------------
 
 
