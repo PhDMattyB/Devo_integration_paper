d88b0ed0ab52eb68a3f7afe358ba452f40591c10
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2025-01-10 14:07:41 +0000

    Create Int_pattern_cor.R

---
+++ Int_pattern_cor.R
@@ -0,0 +1,27 @@
+##############################
+## Integration pattern cor
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 10.01.2025
+##
+##############################
+
+
+setwd('~/Parsons_Postdoc/Stickleback_Morphometric_data/Updated Landmarks/')
+
+
+library(geomorph)
+library(RRPP)
+library(MASS)
+library(ppcor)
+library(igraph)
+library(reshape2)
+library(candisc)
+library(tidyverse)
+library(lineup)
+
+## To run this script make sure Wild_pattern and Plasticity_analysis
+## are run first. This will be calling datasets from them
+
+
