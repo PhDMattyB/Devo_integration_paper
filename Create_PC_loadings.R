83705501339b94466959fe85f18c88eee6e26681
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2026-05-27 18:52:56 +0100

    revamp the process

---
+++ Create_PC_loadings.R
@@ -0,0 +1,208 @@
+##############################
+## PC loading matrices
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 27.05.2026
+##
+##############################
+
+setwd("~/Parsons_postdoc/Stickleback_Morphometric_data/Updated Landmarks/")
+
+library(tidyverse)
+library(lme4)
+library(lmerTest)
+library(brms)
+library(paran)
+library(broom.mixed)
+library(MatrixCorrelation)
+
+
+# functions ---------------------------------------------------------------
+
+cor_fun <- function(data, pc_cols, trait_cols) {
+  
+  expand_grid(
+    pc = pc_cols,
+    trait = trait_cols) %>%
+    mutate(test = map2(pc,trait,
+        \(x, y)
+        cor.test(
+          x = data[[x]],
+          y = data[[y]],
+          method = "pearson") %>%
+          tidy())) %>%
+    unnest(test) %>%
+    transmute(
+      pc,
+      trait,
+      r = estimate,
+      p = p.value)
+}
+# F2 data -----------------------------------------------------------------
+
+F2_data = read_csv("F2_Original_univariate_traits_FIXED_11.02.2026.csv") %>% 
+  dplyr::select(Lake_morph, 
+                Ecotype_pair, 
+                lake_morph_Pair_Full_Temp, 
+                Morph, 
+                Offspring_temp, 
+                Parent_temp, 
+                jaw_length:caudal2_15_17)
+
+
+F2_orig_trait_mat <- F2_data %>%
+  dplyr::select(jaw_length:caudal2_15_17) %>% 
+  # dplyr::select(starts_with('value'))
+  # dplyr::select(all_of(trait_names)) %>%
+  as.matrix()
+
+pca = prcomp(F2_orig_trait_mat, 
+              center = F, 
+              scale. = F)
+
+paran(F2_orig_trait_mat, iterations = 1000, graph = TRUE)
+
+scores = as.data.frame(pca$x[, 1:7])
+F2_PCA = bind_cols(F2_data, scores)
+
+trait_cols = F2_PCA %>% 
+  dplyr::select(7:39) %>% 
+  names()
+
+pc_cols = F2_PCA %>% 
+  dplyr::select(40:46) %>% 
+  names()
+
+F2_WC_loads = F2_PCA %>%
+  group_by(Morph) %>%
+  nest() %>%
+  mutate(
+    correlations = map(
+      data,
+      cor_fun,
+      pc_cols = pc_cols,
+      trait_cols = trait_cols)) %>%
+  dplyr::select(-data) %>%
+  unnest(correlations)
+
+
+F2_WC_loads %>% 
+  filter(pc == 'PC1')
+
+## check graphically
+F2_WC_loads %>%
+  ggplot(aes(pc, 
+             trait, 
+             fill = r)) +
+  geom_tile() +
+  facet_wrap(~Morph) +
+  scale_fill_gradient2(
+    low = "blue",
+    mid = "white",
+    high = "red",
+    limits = c(-1, 1)) +
+  theme(
+    axis.text.y = element_text(size = 7))
+
+F2_cold = F2_WC_loads %>%
+  filter(Morph == "Cold") %>%
+  dplyr::select(pc, trait, r_cold = r)
+
+F2_warm = F2_WC_loads %>%
+  filter(Morph == "Warm") %>%
+  dplyr::select(pc, trait, r_warm = r)
+
+F2_diff_df <- F2_cold %>%
+  inner_join(F2_warm,
+    by = c("pc", "trait")) %>%
+  mutate(diff = r_warm - r_cold)
+
+ggplot(diff_df, 
+       aes(pc,
+           trait,
+           fill = diff)) +
+  geom_tile() +
+  scale_fill_gradient2(
+    low = "blue",
+    mid = "white",
+    high = "red") 
+
+
+F2_cold_mat <- F2_WC_loads %>%
+  ungroup() %>% 
+  filter(Morph == "Cold") %>%
+  dplyr::select(trait, pc, r) %>%
+  pivot_wider(names_from = pc,
+    values_from = r) %>%
+  column_to_rownames("trait") %>%
+  as.matrix()
+
+
+F2_warm_mat <- F2_WC_loads %>%
+  ungroup() %>% 
+  filter(Morph == "Warm") %>%
+  dplyr::select(trait, pc, r) %>%
+  ungroup() %>% 
+  pivot_wider(names_from = pc,
+              values_from = r) %>%
+  column_to_rownames("trait") %>%
+  as.matrix()
+
+
+allCorrelations(
+  F2_cold_mat,
+  F2_warm_mat,
+  ncomp1 = 7,
+  ncomp2 = 7)
+
+perm_test_data = F2_PCA %>% 
+  dplyr::select(Morph, 
+                7:39, 
+                40:46)
+
+
+obs <- RV(F2_cold_mat, F2_warm_mat)
+set.seed(123)
+
+null_rv <- replicate(1000, {
+  
+  # 1. permute group labels in RAW data
+  permuted <- perm_test_data %>%
+    mutate(Morph = sample(Morph))
+  
+  # 2. recompute correlations
+  perm_results <- permuted %>%
+    group_by(Morph) %>%
+    nest() %>%
+    mutate(
+      correlations = map(
+        data,
+        cor_fun,
+        pc_cols = pc_cols,
+        trait_cols = trait_cols)) %>% 
+    # dplyr::select(-data) %>%
+    unnest(correlations)
+  
+  # 3. build matrices
+  cold_perm <- perm_results %>%
+    filter(Morph == "Cold") %>%
+    ungroup() %>% 
+    dplyr::select(trait, pc, r) %>%
+    pivot_wider(names_from = pc, values_from = r) %>%
+    column_to_rownames("trait") %>%
+    as.matrix()
+  
+  warm_perm <- perm_results %>%
+    filter(Morph == "Warm") %>%
+    ungroup() %>% 
+    dplyr::select(trait, pc, r) %>%
+    pivot_wider(names_from = pc, values_from = r) %>%
+    column_to_rownames("trait") %>%
+    as.matrix()
+  
+  # 4. RV statistic
+  RV(cold_perm, warm_perm)
+})
+
+mean(null_rv >= obs)
--- RRPP_Trajectory.R
+++ RRPP_Trajectory.R
@@ -15,6 +15,92 @@ library(paran)
 library(RRPP)
 
 
+
+# STRAIGHT TRAITS HOMIE ---------------------------------------------------
+
+F2_data = read_csv("F2_Original_univariate_traits_FIXED_11.02.2026.csv") %>% 
+  dplyr::select(Lake_morph, 
+                Ecotype_pair, 
+                lake_morph_Pair_Full_Temp, 
+                Morph, 
+                Offspring_temp, 
+                Parent_temp, 
+                jaw_length:caudal2_15_17) %>% 
+  unite(col = off_parent_temp, 
+        c('Offspring_temp', 
+          'Parent_temp'), 
+        sep = '_', 
+        remove = F)
+
+F2_traits = F2_data %>% 
+  dplyr::select(8:40) %>% 
+  as.matrix()
+
+F2_RRPP_mod <- lm.rrpp(F2_traits ~ Morph*Offspring_temp*Parent_temp , 
+                       SS.type = "I", 
+                       data = F2_data, 
+                       print.progress = FALSE, 
+                       iter = 999,
+                       verbose = TRUE) 
+summary(F2_RRPP_mod, 
+        formula = FALSE)
+anova(F2_RRPP_mod) 
+
+coef(F2_RRPP_mod, test = TRUE)
+
+betaTest(F2_RRPP_mod)
+
+F2_off_traj = trajectory.analysis(F2_RRPP_mod, 
+                                  fit.null = NULL, 
+                                  groups = F2_data$Morph, 
+                                  traj.pts = F2_data$Offspring_temp, 
+                                  pca = TRUE, 
+                                  print.progress = FALSE)
+
+summary(F2_off_traj, attribute = "MD", angle.type = "deg", 
+        show.trajectories = T)
+summary(F2_off_traj, attribute = "TC", angle.type = "deg")
+
+F2_parent_traj = trajectory.analysis(F2_RRPP_mod, 
+                                     fit.null = NULL, 
+                                     groups = F2_data$Morph, 
+                                     traj.pts = F2_data$Parent_temp, 
+                                     pca = TRUE, 
+                                     print.progress = FALSE)
+summary(F2_parent_traj, attribute = "MD", angle.type = "deg")
+summary(F2_parent_traj, attribute = "TC", angle.type = "deg")
+
+
+F2_off_parent_traj = trajectory.analysis(F2_RRPP_mod, 
+                                         fit.null = NULL, 
+                                         groups = F2_data$Morph, 
+                                         traj.pts = F2_data$off_parent_temp, 
+                                         pca = TRUE, 
+                                         print.progress = FALSE)
+
+summary(F2_off_parent_traj, 
+        attribute = "MD", 
+        angle.type = "deg", 
+        show.trajectories = T)
+summary(F2_off_parent_traj, 
+        attribute = "TC", 
+        angle.type = "deg", 
+        show.trajectories = T)
+
+F2_groups <- interaction(F2_data$Morph, F2_data$Offspring_temp, F2_data$Parent_temp)
+F2_groups <- interaction(F2_data$Morph, F2_data$Offspring_temp)
+F2_groups <- interaction(F2_data$Morph, F2_data$Parent_temp)
+
+pw_traj <- pairwise(F2_RRPP_mod, 
+                    groups = F2_groups)
+summary(pw_traj)
+
+
+
+# PC axes -----------------------------------------------------------------
+
+
+
 F2_pca_data = read_csv('F2_orig_PCA_sig_axes.csv') %>% 
   unite(col = off_parent_temp, 
         c('Offspring_temp', 
@@ -44,8 +130,6 @@ anova(F2_RRPP_mod)
 coef(F2_RRPP_mod, test = TRUE)
 
 
-
-
 F2_off_traj = trajectory.analysis(F2_RRPP_mod, 
                     fit.null = NULL, 
                     groups = F2_pca_data$Morph, 
@@ -56,16 +140,6 @@ F2_off_traj = trajectory.analysis(F2_RRPP_mod,
 summary(F2_off_traj, attribute = "MD", angle.type = "deg", 
         show.trajectories = T)
 summary(F2_off_traj, attribute = "TC", angle.type = "deg")
-
-plot(F2_off_traj, 
-     pch = as.numeric(F2_pca_data$Morph) + 20,
-     bg = as.numeric(F2_pca_data$Offspring_temp),
-          cex = 0.7, col = "gray")
-add.trajectories(F2_off_traj, 
-                 traj.pch = c(21, 22, 23, 24), 
-                 start.bg = 1, 
-                 end.bg = 2)
-legend("topright", levels(Pupfish$Pop), pch =  c(21, 22), pt.bg = 1)
 
 
 F2_parent_traj = trajectory.analysis(F2_RRPP_mod, 
