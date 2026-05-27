bf25e66b1b2a2b86be3e3f4ae5c57df5e8dbef37
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2026-05-27 15:11:42 +0100

    rrpp mods

--- RRPP_Trajectory.R
+++ RRPP_Trajectory.R
@@ -23,4 +23,17 @@ TGP_pca_data = read_csv('TGP_PCA_sig_axes.csv')
 
 wild_pca_data = read_csv('wild_PCA_sig_axes.csv')
 
+F2_PC_axes = F2_pca_data %>% dplyr::select(PC1:PC7) %>% 
+  as.matrix()
 
+F2_RRPP_mod <- lm.rrpp(F2_PC_axes ~ Morph*Offspring_temp*Parent_temp , 
+                       SS.type = "I", 
+               data = F2_pca_data, 
+               print.progress = FALSE, 
+               iter = 999,
+               verbose = TRUE) 
+summary(F2_RRPP_mod, 
+        formula = FALSE)
+anova(F2_RRPP_mod) 
+
+coef(F2_RRPP_mod, test = TRUE)
